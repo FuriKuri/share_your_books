fbe47b7be72c870f6496d1adcbaecb0faff94928
Theo Pack <tf.pack@gmail.com>
2012-10-10 09:13:07 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ShareYourBooks::Application.config.secret_token = '6ee3eccdb28d8e66bdb1e14da0125490528e396941bb517b2857b64cd7dd39004dd492aff942ffea5f1899a2093785d370a2a0b81a08bf0c0578e213b39ba91f'
