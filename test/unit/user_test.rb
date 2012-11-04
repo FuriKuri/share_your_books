c8b487d2e4edd6eb78bb0a8e62696447eacd4b91
Theo Pack <tf.pack@gmail.com>
2012-11-04 22:27:44 +0100

    use invalid? method to test user object

--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -8,28 +8,28 @@ class UserTest < ActiveSupport::TestCase
     user.name = 'hans'
     user.password = 'secret'
     user.email = 'hans@email.com'
-    assert user.save
+    assert user.valid?
   end
 
   test "should not save user with no name" do
     user = User.new
     user.password = 'secret'
     user.email = 'user@email.com'
-    assert !user.save
+    assert user.invalid?
   end
 
   test "should not save user with no password" do
     user = User.new
     user.name = 'hans'
     user.email = 'user@email.com'
-    assert !user.save
+    assert user.invalid?
   end
 
   test "should not save user with no email" do
     user = User.new
     user.name = 'hans'
     user.password = 'secret'
-    assert !user.save
+    assert user.invalid?
   end
 
   test "should not save user with too short name" do
@@ -37,7 +37,7 @@ class UserTest < ActiveSupport::TestCase
     user.name = 'u'
     user.password = 'secret'
     user.email = 'user@email.com'
-    assert !user.save
+    assert user.invalid?
   end
 
 
@@ -48,7 +48,7 @@ class UserTest < ActiveSupport::TestCase
     user.name = name
     user.password = 'secret'
     user.email = 'user@email.com'
-    assert !user.save
+    assert user.invalid?
   end
 
   test "should not save user with too short password" do
@@ -56,7 +56,7 @@ class UserTest < ActiveSupport::TestCase
     user.name = 'hans'
     user.password = 'pw'
     user.email = 'user@email.com'
-    assert !user.save
+    assert user.invalid?
   end
 
   test "should not save user with too long password" do
@@ -66,7 +66,7 @@ class UserTest < ActiveSupport::TestCase
     41.times {pw += 'p'}
     user.password = pw
     user.email = 'user@email.com'
-    assert !user.save
+    assert user.invalid?
   end
 
   test "should not save user with a wrong email format" do
@@ -74,7 +74,7 @@ class UserTest < ActiveSupport::TestCase
     user.name = 'hans'
     user.password = 'secret'
     user.email = 'email.com'
-    assert !user.save
+    assert user.invalid?
   end
 
   test "should not save user with an existing name" do
@@ -82,7 +82,7 @@ class UserTest < ActiveSupport::TestCase
     user.name = 'user1'
     user.password = 'secret'
     user.email = 'user@email.com'
-    assert !user.save
+    assert user.invalid?
   end
 
   test "should not save user with an existing email" do
@@ -90,7 +90,7 @@ class UserTest < ActiveSupport::TestCase
     user.name = 'hans'
     user.password = 'secret'
     user.email = 'user1@email.com'
-    assert !user.save
+    assert user.invalid?
   end
 
   # test "the truth" do
